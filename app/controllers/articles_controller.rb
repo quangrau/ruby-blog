62b0bd41d4b64ccae672ea71f553c64d288c9007
QuangRau <lethanhquang910@gmail.com>
2014-07-13 01:14:55 +0700

    Add Comment controller, Association, basic auth

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,21 +1,49 @@
 class ArticlesController < ApplicationController
 
-	def new
-	end
+	http_basic_authenticate_with name: "quangrau", password: "123456",
+		expect: [:index, :show]
 
-	def create
-		@article = Article.new(article_params)
-
-		@article.save
-		redirect_to @article
+	def index
+		@articles = Article.all
 	end
 
 	def show
 		@article = Article.find(params[:id])
 	end
 
-	def index
-		@articles = Article.all
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+
+		if @article.save
+			redirect_to @article
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+
+	def update
+		@article = Article.find(params[:id])
+
+		if article.update(article_params)
+			redirect_to @articles
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+
+		redirect_to articles_path
 	end
 
 	private
